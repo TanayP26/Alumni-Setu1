09c73d811c67ba55865423b081c3cf4597896119
Tanay Prasad <tanayprasad750@gmail.com>
2025-09-19 00:08:13 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -5,11 +5,11 @@ Created on Thu Sep 11 17:02:00 2025
 @author: Admin
 """
 
-
+from extensions import db  # Import db, and other extensions from extensions.py
+from models import User, UserRole  # Import models normally
 from flask import Blueprint, request, jsonify, current_app
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from werkzeug.security import check_password_hash
-from models import User, UserRole, db
 from datetime import datetime
 import re
 
@@ -206,3 +206,4 @@ def change_password():
     except Exception as e:
         db.session.rollback()
         return jsonify({'error': str(e)}), 500
+
