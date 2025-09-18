ea5270a4604d5ad6ae89c42acb4c0281374312bf
Tanay Prasad <tanayprasad750@gmail.com>
2025-09-19 00:18:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -74,8 +74,7 @@ def create_app():
     from routes.messages import messages_bp
     from routes.admin import admin_bp
     from routes.auth import auth_bp
-    app.register_blueprint(auth_bp)
-
+    
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(alumni_bp, url_prefix='/api/alumni')
     app.register_blueprint(events_bp, url_prefix='/api/events')
@@ -130,6 +129,7 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
 
 
 
