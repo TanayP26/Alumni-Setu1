141deab7e19ef608de356e2905d137169cd05027
Tanay Prasad <tanayprasad750@gmail.com>
2025-09-21 16:05:06 +0530

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Sep 11 17:21:04 2025
-
 @author: Admin
 """
-
 # routes/__init__.py
+
 from .auth import auth_bp
 from .alumni import alumni_bp
 from .events import events_bp
 from .jobs import jobs_bp
-from .messages import messages_bp
+from .messaging import messaging_bp  # corrected import for messaging routes
 from .admin import admin_bp
 
-__all__ = ['auth_bp', 'alumni_bp', 'events_bp', 'jobs_bp', 'messages_bp', 'admin_bp']
+__all__ = ['auth_bp', 'alumni_bp', 'events_bp', 'jobs_bp', 'messaging_bp', 'admin_bp']
