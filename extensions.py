a17488846f399b1f125b17327337a422ebe3c6da
Tanay Prasad <tanayprasad750@gmail.com>
2025-09-21 16:10:37 +0530

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_socketio import SocketIO
 
 db = SQLAlchemy()
 jwt = JWTManager()
 mail = Mail()
 migrate = Migrate()
+socketio = SocketIO()
